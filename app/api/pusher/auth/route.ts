fc10d71abe7d2ea1354ccce77c659b5b50cda347
iOmKumar30 <iomkumar03@outlook.com>
2025-07-31 05:25:35 +0530

    pusher formdata parse

--- app/api/pusher/auth/route.ts
+++ app/api/pusher/auth/route.ts
@@ -1,35 +1,33 @@
 import { authOptions } from "@/libs/auth";
 import { pusherServer } from "@/libs/pusher";
 import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   const session = await getServerSession(authOptions);
 
   if (!session?.user?.email) {
-    return new NextResponse("Not authenticated", {
-      status: 401,
-      headers: {
-        "Access-Control-Allow-Origin": "*", 
-        "Access-Control-Allow-Headers": "Content-Type",
-      },
+    return new NextResponse("Not authenticated", { status: 401 });
+  }
+
+  const formData = await req.formData();
+  const socketId = formData.get("socket_id") as string;
+  const channel = formData.get("channel_name") as string;
+
+  if (!socketId || !channel) {
+    return new NextResponse("Missing socket_id or channel_name", {
+      status: 400,
     });
   }
 
-  const body = await req.json();
-  const socketId = body.socket_id;
-  const channel = body.channel_name;
-
-  const data = {
+  const authResponse = pusherServer.authorizeChannel(socketId, channel, {
     user_id: session.user.email,
-  };
-
-  const authResponse = pusherServer.authorizeChannel(socketId, channel, data);
+  });
 
   return new NextResponse(JSON.stringify(authResponse), {
+    status: 200,
     headers: {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
     },
   });
 }
